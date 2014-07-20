cbab02437adee709365231751528195563a4a30b
harry159821 <harry159821@gmail.com>
2014-07-20 20:54:37 +0800

    re commit

---
+++ main.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import urllib2
+import json,sys,re
+import webbrowser
+#from wox import WoxAPI
+true = True
+null = None
+false = False
+
+def query(Allkey):
+	Allkey = Allkey.encode('utf-8')
+	key = Allkey.split(" ")[1]
+	if not key:
+		return ""
+	results = []		
+	if not Allkey.endswith(' '):	
+		preKeyUrl = 'http://suggestion.baidu.com/su?json=1&zxmode=1&wd='+key
+		html = requests(preKeyUrl).decode('gbk').encode('utf-8')
+		html = json.loads(html[17:-2])
+		if not html:
+			return ''
+		if html.has_key('tzx'):
+			res = {}
+			res["Title"] = html['tzx']['info']['site']
+			res["SubTitle"] = html['tzx']['info']['showurl']
+			res["ActionName"] = "openUrl"
+			if(download(html['tzx']['info']['iconurl'],sys.path[0]+'\\'+key+'.png')):
+				res["IcoPath"] = sys.path[0]+'\\'+key+'.png'
+			else:
+				res["IcoPath"] = "./icon.png"
+			res["ActionPara"] = html['tzx']['info']['siteurl']
+			results.append(res)
+		for i in html['s']:
+			res = {}
+			res["Title"] = i
+			res["IcoPath"] = "./baidu.png"
+			#res["ActionName"] = "Change"
+			#res["ActionPara"] = i
+			results.append(res)
+		return json.dumps(results)
+	else:
+		url = 'http://wap.baidu.com/s?word='+key
+		html = requests(url)
+		if not html:
+			return ''		
+		html = html.replace('<em>','')
+		html = html.replace('</em>','')	
+		html = html.replace('&#160;','')
+		data = re.findall(r'''(resitem"([\w\W]*?)abs([\w\W]*?)</div>).*?''',html)
+		for item in data:
+			res = {}
+			result = re.findall(r'''(href="([\w\W]*?)">([\w\W]*?)</a><div class="abs">([\w\W]*?)<br /><span class="site">([\w\W]*?)</span>([\w\W]*?)date">([\w\W]*?)</span>).*?''',item[0])
+			if result:
+				try:
+					res["Title"] = result[0][2]
+					res["SubTitle"] = result[0][3]
+					res["ActionName"] = "openUrl2"
+					res["IcoPath"] = "./icon.png"
+					res["ActionPara"] = 'http://wap.baidu.com'+result[0][1].replace('amp;','')
+					results.append(res)
+				except Exception, e:
+					print e			
+		return json.dumps(results)
+
+def requests(url,timeouts=4):
+	header = {
+			'Referer': 'http://www.baidu.com/',
+			'User-Agent': 'User-Agent:Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.72 Safari/537.36',
+			}	
+	request = urllib2.Request(url,headers=header)
+	response = urllib2.urlopen(request,timeout=timeouts)
+	html = response.read()
+	if html:	
+		return html
+	return False
+
+def getRealUrl(url):
+	url = url.replace('amp;','')
+	header = {
+			'Referer': 'http://wap.baidu.com/',
+			'User-Agent': 'User-Agent:Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.72 Safari/537.36',
+			}	
+	request = urllib2.Request(url,headers=header)	
+	html = urllib2.urlopen(request,timeout=8).read()	
+	result = re.findall(r'''(url=([\w\W]*?)">原网页).*?''',html)
+	if result:
+		return result[0][1]
+	return url
+
+
+def download(url,path):
+	File = requests(url)
+	if not File:
+		return False
+	FILE = open(path,'wb')
+	FILE.write(File)
+	FILE.close()
+	return True
+
+def openUrl(context,url):	
+	webbrowser.open(url)
+
+#def Change(context,text):
+#	WoxAPI.change_query(text)
+
+def openUrl2(context,url):
+	webbrowser.open(getRealUrl(url))
+
+if __name__ == '__main__':
+	#print query(u"search xiami ")
+	print getRealUrl("http://wap.baidu.com/ssid=0/from=0/bd_page_type=1/uid=0/baiduid=45075F32CD2672D6989AD5727F9582B4/pu=sz%40224_220%2Cta%40middle____/baiduid=45075F32CD2672D6989AD5727F9582B4/w=0_10_xiami/t=wap/l=0/tc?ref=www_colorful&lid=5167244119924064437&order=9&vit=osres&tj=www_normal_9_0_10&sec=40091&di=8099560a9bcc75e7&bdenc=1&nsrc=IlPT2AEptyoA_yixCFOxXnANedT62v3IHti2LC6N_8SxokDyqRLuFM2bXCP7Lny")
---
+++ result.py
@@ -0,0 +1,82 @@
+window.baidu.sug({"q":"123","p":false,
+		 			 "s":[
+		 			       "12306",
+		 			       "12306铁路客户服务中心",
+		 			       "12306火车票网上订票",
+		 			       "12306网上订火车票",
+		 			       "123网址之家",
+		 			       "12333",
+		 			       "12306官网",
+		 			       "12360",
+		 			       "12306订票助手",
+		 			       "12333社保查询网"],
+		 			"zzx":["0","0","0","0","0","0","0","0","0","0"],
+		 			"tzx":{"type":"6", 
+		 				   "info":{"site":"hao123导航", 
+		 				   	 	   "showurl":"www.hao123.com", 
+		 				   	 	   "siteurl":"http://www.hao123.com/", 
+		 				   	 	   "iconurl":"http://t10.baidu.com/it/u=509080071,3205022283&fm=58", 
+		 				   	 	   "hit_q":"123"}
+		 				   	 	   }
+		 				   }
+		 			);
+
+				{u'q': u'12', u'p': False, 
+					u's': [	u'12306', 
+						   	u'126', 
+						   	u'12306\u94c1\u8def\u5ba2\u6237\u670d\u52a1\u4e2d\u5fc3', 
+						   	u'126\u90ae\u7bb1\u767b\u9646', 
+						   	u'12306\u706b\u8f66\u7968\u7f51\u4e0a\u8ba2\u7968', 
+						   	u'123', 
+						   	u'12306\u7f51\u4e0a\u8ba2\u706b\u8f66\u7968', 
+						   	u'126\u90ae\u7bb1', 
+						   	u'126 \u514d\u8d39\u90ae\u7bb1', 
+						   	u'123\u7f51\u5740\u4e4b\u5bb6'], 
+					u'zzx':[u'0', u'0', u'0', u'0', u'0', u'0', u'0', u'0', u'0', u'0']
+					}
+
+
+[	
+	{	
+		"ActionName": "openUrl", 
+		"SubTitle": "www.hao123.com", 
+		"IcoPath": "./123.png", 
+		"ActionPara": "http://www.hao123.com/", 
+		"Title": "hao123\u5bfc\u822a"
+	}, 
+
+	{	
+		"IcoPath": "./baidu.png", 
+		"Title": "12306"
+	}, 
+
+	{	
+		"IcoPath": "./baidu.png", 
+		"Title": "12306\u94c1\u8def\u5ba2\u6237\u670d\u52a1\u4e2d\u5fc3"
+	}, 
+
+	{	
+		"IcoPath": "./baidu.png", 
+		"Title": "12306\u706b\u8f66\u7968\u7f51\u4e0a\u8ba2\u7968"
+	},
+
+	{	
+		"IcoPath": "./baidu.png", 
+		"Title": "12306\u7f51\u4e0a\u8ba2\u706b\u8f66\u7968"
+	}, 
+
+	{	
+		"IcoPath": "./baidu.png", 
+		"Title": "123\u7f51\u5740\u4e4b\u5bb6"
+	}, 
+
+	{		
+		"IcoPath": "./baidu.png", 
+		"Title": "12333"
+	}, 
+
+	{	
+		"IcoPath": "./baidu.png", 
+		"Title": "12306\u5b98\u7f51"
+	}, {"IcoPath": "./baidu.png", "Title": "12360"}, {"IcoPath": "./baidu.png", "Title": "12306\u8ba2\u7968\u52a9\u624b"}, {"IcoPath": "./baidu.png", "Title": "12333\u793e\u4fdd\u67e5\u8be2\u7f51"}
+]
